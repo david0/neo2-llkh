c58580797f18b9fef20a38b5dfc556e43346932a
David <ottodavid@gmx.net>
2014-07-22 17:54:11 +0200

    Formatted source code

--- main.c
+++ main.c
@@ -6,108 +6,116 @@
 
 HHOOK keyhook = NULL;
 
-TCHAR mapKey(unsigned level, char in) {
-  unsigned len=103;
-  TCHAR mappingTable[len];
-  for(int i=0; i<len; i++)
-    mappingTable[i]=0;
-
-  switch(level) {
-  case 1:
-  case 2:
-    wcscpy(mappingTable+16, L"xvlcwkhgfqß");
-    wcscpy(mappingTable+30, L"uiaeosnrtdy");
-    wcscpy(mappingTable+44, L"üöäpzbm,.j");
-    break;
-  case 3:
-    wcscpy(mappingTable+16, L"…_[]^!<>=&");
-    wcscpy(mappingTable+30, L"\\/{}*?()-:@");
-    wcscpy(mappingTable+44, L"#$|~`+%\"';");
-    break;
-  case 4:
-    wcscpy(mappingTable+16, L"       789-");
-    wcscpy(mappingTable+30, L"       456,");
-    wcscpy(mappingTable+44, L"       123;");
-    break;
-  }
-  return mappingTable[in];
-}
-
-
-void sendChar(TCHAR key, KBDLLHOOKSTRUCT keyinfo) 
+TCHAR mapKey(unsigned level, char in)
 {
-      SHORT extinfo = VkKeyScanEx(key,GetKeyboardLayout(0)); 
-      keyinfo.vkCode=extinfo;
-      char modifiers = extinfo >> 8;
-      bool shift = ((modifiers & 1) != 0);
-      bool alt = ((modifiers & 2) != 0);
-      bool ctrl = ((modifiers & 4) != 0);
+	unsigned len = 103;
+	TCHAR mappingTable[len];
+	for (int i = 0; i < len; i++)
+		mappingTable[i] = 0;
 
-      if(shift) keybd_event(VK_SHIFT,0,0,0);
-      if(alt) keybd_event(VK_MENU,0,0,0); // ALT
-      if(ctrl) keybd_event(VK_CONTROL,0,0,0);
-
-      keybd_event(keyinfo.vkCode, keyinfo.scanCode, keyinfo.flags, keyinfo.dwExtraInfo);
-      if(shift) keybd_event(VK_SHIFT,0,KEYEVENTF_KEYUP,0);
-      if(alt) keybd_event(VK_MENU,0,KEYEVENTF_KEYUP,0); // ALT
-      if(ctrl) keybd_event(VK_CONTROL,0,KEYEVENTF_KEYUP,0);
+	switch (level) {
+	case 1:
+	case 2:
+		wcscpy(mappingTable + 16, L"xvlcwkhgfqß");
+		wcscpy(mappingTable + 30, L"uiaeosnrtdy");
+		wcscpy(mappingTable + 44, L"üöäpzbm,.j");
+		break;
+	case 3:
+		wcscpy(mappingTable + 16, L"…_[]^!<>=&");
+		wcscpy(mappingTable + 30, L"\\/{}*?()-:@");
+		wcscpy(mappingTable + 44, L"#$|~`+%\"';");
+		break;
+	case 4:
+		wcscpy(mappingTable + 16, L"       789-");
+		wcscpy(mappingTable + 30, L"       456,");
+		wcscpy(mappingTable + 44, L"       123;");
+		break;
+	}
+	return mappingTable[in];
 }
 
-
-bool isMod3(KBDLLHOOKSTRUCT keyinfo) {
-	return keyinfo.vkCode == VK_CAPITAL || keyinfo.scanCode == 541|| keyinfo.scanCode==43;
-}
-
-bool isMod4(KBDLLHOOKSTRUCT keyinfo) {
-	return keyinfo.scanCode==43;
-}
-
-
-__declspec(dllexport) LRESULT CALLBACK keyevent(int code, WPARAM wparam, LPARAM lparam)
+void sendChar(TCHAR key, KBDLLHOOKSTRUCT keyinfo)
 {
-  static bool mod3Pressed=false;
-  static bool mod4Pressed=false;
+	SHORT extinfo = VkKeyScanEx(key, GetKeyboardLayout(0));
+	keyinfo.vkCode = extinfo;
+	char modifiers = extinfo >> 8;
+	bool shift = ((modifiers & 1) != 0);
+	bool alt = ((modifiers & 2) != 0);
+	bool ctrl = ((modifiers & 4) != 0);
 
-	if(code == HC_ACTION && (wparam == WM_SYSKEYUP || wparam == WM_KEYUP)) {
-		KBDLLHOOKSTRUCT keyinfo = *((KBDLLHOOKSTRUCT *)lparam);
-    printf("Keyhook up %u, %u %x %d\n", code, keyinfo.scanCode,keyinfo.flags, keyinfo.dwExtraInfo);
-    if(isMod3(keyinfo)) {
-        mod3Pressed=false;
-				return -1;
-    } else if(isMod4(keyinfo)){
-        mod4Pressed=false;
-				return -1;
+	if (shift)
+		keybd_event(VK_SHIFT, 0, 0, 0);
+	if (alt)
+		keybd_event(VK_MENU, 0, 0, 0);	// ALT
+	if (ctrl)
+		keybd_event(VK_CONTROL, 0, 0, 0);
+
+	keybd_event(keyinfo.vkCode, keyinfo.scanCode, keyinfo.flags, keyinfo.dwExtraInfo);
+	if (shift)
+		keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
+	if (alt)
+		keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0);	// ALT
+	if (ctrl)
+		keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+}
+
+bool isMod3(KBDLLHOOKSTRUCT keyinfo)
+{
+	return keyinfo.vkCode == VK_CAPITAL || keyinfo.scanCode == 541 || keyinfo.scanCode == 43;
+}
+
+bool isMod4(KBDLLHOOKSTRUCT keyinfo)
+{
+	return keyinfo.scanCode == 43;
+}
+
+__declspec(dllexport)
+LRESULT CALLBACK keyevent(int code, WPARAM wparam, LPARAM lparam)
+{
+	static bool mod3Pressed = false;
+	static bool mod4Pressed = false;
+
+	if (code == HC_ACTION && (wparam == WM_SYSKEYUP || wparam == WM_KEYUP)) {
+		KBDLLHOOKSTRUCT keyinfo = *((KBDLLHOOKSTRUCT *) lparam);
+		printf("Keyhook up %u, %u %x %d\n", code, keyinfo.scanCode, keyinfo.flags,
+		       keyinfo.dwExtraInfo);
+		if (isMod3(keyinfo)) {
+			mod3Pressed = false;
+			return -1;
+		} else if (isMod4(keyinfo)) {
+			mod4Pressed = false;
+			return -1;
 		}
-  }
+	}
 
-	else if (code == HC_ACTION && (wparam == WM_SYSKEYDOWN || wparam == WM_KEYDOWN) ) {
-		KBDLLHOOKSTRUCT keyinfo = *((KBDLLHOOKSTRUCT *)lparam);
+	else if (code == HC_ACTION && (wparam == WM_SYSKEYDOWN || wparam == WM_KEYDOWN)) {
+		KBDLLHOOKSTRUCT keyinfo = *((KBDLLHOOKSTRUCT *) lparam);
 
-    unsigned level=1; 
-    if(mod3Pressed)
-      level=3;
-    if(mod4Pressed)
-      level=4;
+		unsigned level = 1;
+		if (mod3Pressed)
+			level = 3;
+		if (mod4Pressed)
+			level = 4;
 
-    printf("Keyhook: %u, %u %x %d\n", code, keyinfo.scanCode,keyinfo.flags, keyinfo.dwExtraInfo); 
+		printf("Keyhook: %u, %u %x %d\n", code, keyinfo.scanCode, keyinfo.flags,
+		       keyinfo.dwExtraInfo);
 
-    TCHAR key = mapKey(level, keyinfo.scanCode);
-    if(key!=0 && (keyinfo.flags & LLKHF_INJECTED)==0) {
+		TCHAR key = mapKey(level, keyinfo.scanCode);
+		if (key != 0 && (keyinfo.flags & LLKHF_INJECTED) == 0) {
 			// if key must be mapped
-   		printf("Mapped %d->%c (level %u)\n", keyinfo.scanCode, key, level);
-      sendChar(key, keyinfo);
-	    return -1;
-    } else if(isMod3(keyinfo)) {
-      mod3Pressed=true; 
-      return -1;  
-    } else if( isMod4(keyinfo) )  {
-      mod4Pressed=true; 
-      return -1;  
+			printf("Mapped %d->%c (level %u)\n", keyinfo.scanCode, key, level);
+			sendChar(key, keyinfo);
+			return -1;
+		} else if (isMod3(keyinfo)) {
+			mod3Pressed = true;
+			return -1;
+		} else if (isMod4(keyinfo)) {
+			mod4Pressed = true;
+			return -1;
 		}
 	}
 	return CallNextHookEx(NULL, code, wparam, lparam);
 }
-
 
 DWORD WINAPI driver(void *user)
 {
@@ -115,7 +123,7 @@ DWORD WINAPI driver(void *user)
 	MSG msg;
 
 	if (!base) {
-		if (!(base = LoadLibrary((wchar_t *)user))) {
+		if (!(base = LoadLibrary((wchar_t *) user))) {
 			return 1;
 		}
 	}
@@ -143,4 +151,3 @@ int main(int argc, char *argv[])
 	}
 	return 0;
 }
-
