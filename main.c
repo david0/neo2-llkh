2380b290e6c38f6bd71567c335e1da0fd8fe064f
David <david0@github.com>
2014-07-22 11:05:18 +0200

    2nd mod3

--- main.c
+++ main.c
@@ -12,7 +12,6 @@ TCHAR mapKey(unsigned level, char in) {
   for(int i=0; i<len; i++)
     mappingTable[i]=0;
 
-  
   switch(level) {
   case 1:
   case 2:
@@ -35,47 +34,54 @@ TCHAR mapKey(unsigned level, char in) {
 }
 
 
-void sendChar(TCHAR key) 
+void sendChar(TCHAR key, KBDLLHOOKSTRUCT keyinfo) 
 {
-      SHORT extinfo =VkKeyScanEx(key,GetKeyboardLayout(0)); 
-      KBDLLHOOKSTRUCT keyinfo;
+      SHORT extinfo = VkKeyScanEx(key,GetKeyboardLayout(0)); 
       keyinfo.vkCode=extinfo;
       char modifiers = extinfo >> 8;
       bool shift = ((modifiers & 1) != 0);
       bool alt = ((modifiers & 2) != 0);
       bool ctrl = ((modifiers & 4) != 0);
 
-
-
       if(shift) keybd_event(VK_SHIFT,0,0,0);
       if(alt) keybd_event(VK_MENU,0,0,0); // ALT
       if(ctrl) keybd_event(VK_CONTROL,0,0,0);
 
       keybd_event(keyinfo.vkCode, keyinfo.scanCode, keyinfo.flags, keyinfo.dwExtraInfo);
-
       if(shift) keybd_event(VK_SHIFT,0,KEYEVENTF_KEYUP,0);
       if(alt) keybd_event(VK_MENU,0,KEYEVENTF_KEYUP,0); // ALT
       if(ctrl) keybd_event(VK_CONTROL,0,KEYEVENTF_KEYUP,0);
 }
 
 
+bool isMod3(KBDLLHOOKSTRUCT keyinfo) {
+	return keyinfo.vkCode == VK_CAPITAL || keyinfo.scanCode == 541|| keyinfo.scanCode==43;
+}
+
+bool isMod4(KBDLLHOOKSTRUCT keyinfo) {
+	return keyinfo.scanCode==43;
+}
 
 
 __declspec(dllexport) LRESULT CALLBACK keyevent(int code, WPARAM wparam, LPARAM lparam)
 {
-    static bool mod3Pressed=false;
-    static bool mod4Pressed=false;
+  static bool mod3Pressed=false;
+  static bool mod4Pressed=false;
+
 	if(code == HC_ACTION && (wparam == WM_SYSKEYUP || wparam == WM_KEYUP)) {
 		KBDLLHOOKSTRUCT keyinfo = *((KBDLLHOOKSTRUCT *)lparam);
-    if(keyinfo.scanCode == VK_CAPITAL || keyinfo.scanCode == 541)
+    printf("Keyhook up %u, %u %x %d\n", code, keyinfo.scanCode,keyinfo.flags, keyinfo.dwExtraInfo);
+    if(isMod3(keyinfo)) {
         mod3Pressed=false;
-    if(keyinfo.scanCode == 43)
+				return -1;
+    } else if(isMod4(keyinfo)){
         mod4Pressed=false;
+				return -1;
+		}
   }
-	if (code == HC_ACTION && (wparam == WM_SYSKEYDOWN || wparam == WM_KEYDOWN) 
-) {
+
+	else if (code == HC_ACTION && (wparam == WM_SYSKEYDOWN || wparam == WM_KEYDOWN) ) {
 		KBDLLHOOKSTRUCT keyinfo = *((KBDLLHOOKSTRUCT *)lparam);
-		DWORD m;
 
     unsigned level=1; 
     if(mod3Pressed)
@@ -83,23 +89,22 @@ __declspec(dllexport) LRESULT CALLBACK keyevent(int code, WPARAM wparam, LPARAM 
     if(mod4Pressed)
       level=4;
 
-    printf("Keyhook: %u, %u %x %s\n", code, keyinfo.scanCode,keyinfo.flags, keyinfo.dwExtraInfo); 
+    printf("Keyhook: %u, %u %x %d\n", code, keyinfo.scanCode,keyinfo.flags, keyinfo.dwExtraInfo); 
+
     TCHAR key = mapKey(level, keyinfo.scanCode);
     if(key!=0 && (keyinfo.flags & LLKHF_INJECTED)==0) {
-        		printf("%d->%c (level %u)\n", keyinfo.scanCode, key, level);
-      sendChar(key);
+			// if key must be mapped
+   		printf("Mapped %d->%c (level %u)\n", keyinfo.scanCode, key, level);
+      sendChar(key, keyinfo);
 	    return -1;
-    } else if( keyinfo.vkCode == VK_CAPITAL||keyinfo.scanCode==541)  {
+    } else if(isMod3(keyinfo)) {
       mod3Pressed=true; 
       return -1;  
-    } else if( keyinfo.scanCode==43)  {
+    } else if( isMod4(keyinfo) )  {
       mod4Pressed=true; 
       return -1;  
-     } else
-	    return CallNextHookEx(NULL, code, wparam, lparam);
-
+		}
 	}
-  else
 	return CallNextHookEx(NULL, code, wparam, lparam);
 }
 
