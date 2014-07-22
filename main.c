5024299368f80fa688c68d40fa2e33f1ef74341b
David <david0@github.com>
2014-07-22 19:03:39 +0200

    fixed shift behavior

--- main.c
+++ main.c
@@ -15,10 +15,15 @@ TCHAR mapKey(unsigned level, char in)
 
 	switch (level) {
 	case 1:
-	case 2:
 		wcscpy(mappingTable + 16, L"xvlcwkhgfqß");
 		wcscpy(mappingTable + 30, L"uiaeosnrtdy");
 		wcscpy(mappingTable + 44, L"üöäpzbm,.j");
+		break;
+	case 2:
+		wcscpy(mappingTable + 2, L"°§ℓ»«$€„“”—");
+		wcscpy(mappingTable + 16, L"XVLCWKHGFQSS");
+		wcscpy(mappingTable + 30, L"UIAEOSNRTDY");
+		wcscpy(mappingTable + 44, L"ÜÖÄPZBM–•J");
 		break;
 	case 3:
 		wcscpy(mappingTable + 2, L"¹²³›‹¢¥‚‘’‐");
@@ -27,11 +32,10 @@ TCHAR mapKey(unsigned level, char in)
 		wcscpy(mappingTable + 44, L"#$|~`+%\"';");
 		break;
 	case 4:
-		wcscpy(mappingTable + 2, L"°§ℓ»«$€„“”—");
-		wcscpy(mappingTable + 16, L"…_[]^!<>=&");
-		wcscpy(mappingTable + 33, L"789-");
-		wcscpy(mappingTable + 37, L"456,");
-		wcscpy(mappingTable + 51, L"123;");
+		wcscpy(mappingTable + 2, L"ªº№⋮·£¤\0/*-");
+		wcscpy(mappingTable + 21, L"¡789+−");
+		wcscpy(mappingTable + 35, L"¿456,.");
+		wcscpy(mappingTable + 49, L":123;");
 		break;
 	}
 	return mappingTable[in];
@@ -54,12 +58,18 @@ void sendChar(TCHAR key, KBDLLHOOKSTRUCT keyInfo)
 		keybd_event(VK_CONTROL, 0, 0, 0);
 
 	keybd_event(keyInfo.vkCode, keyInfo.scanCode, keyInfo.flags, keyInfo.dwExtraInfo);
+
 	if (shift)
 		keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
 	if (alt)
 		keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0);	// ALT
 	if (ctrl)
 		keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+}
+
+bool isShift(KBDLLHOOKSTRUCT keyInfo)
+{
+	return keyInfo.vkCode == VK_SHIFT || keyInfo.vkCode == VK_LSHIFT || keyInfo.vkCode == VK_RSHIFT  ;
 }
 
 bool isMod3(KBDLLHOOKSTRUCT keyInfo)
@@ -69,20 +79,34 @@ bool isMod3(KBDLLHOOKSTRUCT keyInfo)
 
 bool isMod4(KBDLLHOOKSTRUCT keyInfo)
 {
-	return keyInfo.scanCode == 43;
+	return keyInfo.scanCode == 43 || keyInfo.scanCode == 86;
 }
 
 __declspec(dllexport)
 LRESULT CALLBACK keyevent(int code, WPARAM wparam, LPARAM lparam)
 {
+	static bool shiftPressed = false;
 	static bool mod3Pressed = false;
 	static bool mod4Pressed = false;
 
+
+	KBDLLHOOKSTRUCT keyInfo;
+	if (code == HC_ACTION && (wparam == WM_SYSKEYUP || wparam == WM_KEYUP || wparam == WM_SYSKEYDOWN || wparam == WM_KEYDOWN)) 
+	{
+		keyInfo = *((KBDLLHOOKSTRUCT *) lparam);
+		if (keyInfo.flags & LLKHF_INJECTED) // process injected events like normal, because most probably we are injecting them
+			return CallNextHookEx(NULL, code, wparam, lparam);
+	}
+
 	if (code == HC_ACTION && (wparam == WM_SYSKEYUP || wparam == WM_KEYUP)) {
-		KBDLLHOOKSTRUCT keyInfo = *((KBDLLHOOKSTRUCT *) lparam);
-		printf("Keyhook up %u, %u %x %d\n", code, keyInfo.scanCode, keyInfo.flags,
+		printf("Keyhook up %u, sc %u vk %x %x %d\n", code, keyInfo.scanCode, keyInfo.vkCode, keyInfo.flags,
 		       keyInfo.dwExtraInfo);
-		if (isMod3(keyInfo)) {
+
+		if(isShift(keyInfo)) {
+			shiftPressed = false;
+			keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
+			return -1;
+		} else if (isMod3(keyInfo)) {
 			mod3Pressed = false;
 			return -1;
 		} else if (isMod4(keyInfo)) {
@@ -92,22 +116,30 @@ LRESULT CALLBACK keyevent(int code, WPARAM wparam, LPARAM lparam)
 	}
 
 	else if (code == HC_ACTION && (wparam == WM_SYSKEYDOWN || wparam == WM_KEYDOWN)) {
-		KBDLLHOOKSTRUCT keyInfo = *((KBDLLHOOKSTRUCT *) lparam);
-
 		unsigned level = 1;
+		if (shiftPressed)
+			level = 2;
 		if (mod3Pressed)
 			level = 3;
 		if (mod4Pressed)
 			level = 4;
 
-		printf("Keyhook: %u, %u %x %d\n", code, keyInfo.scanCode, keyInfo.flags,
+		printf("Keyhook %u, sc %u vk %x %x %d\n", code, keyInfo.scanCode, keyInfo.vkCode, keyInfo.flags,
 		       keyInfo.dwExtraInfo);
 
 		TCHAR key = mapKey(level, keyInfo.scanCode);
 		if (key != 0 && (keyInfo.flags & LLKHF_INJECTED) == 0) {
 			// if key must be mapped
 			printf("Mapped %d->%c (level %u)\n", keyInfo.scanCode, key, level);
+			//BYTE state[256];
+			//GetKeyboardState(state);
 			sendChar(key, keyInfo);
+			//SetKeyboardState(state);
+
+			return -1;
+		} else if (isShift(keyInfo)) {
+			shiftPressed = true;
+			keybd_event(VK_SHIFT, 0, 0, 0);
 			return -1;
 		} else if (isMod3(keyInfo)) {
 			mod3Pressed = true;
